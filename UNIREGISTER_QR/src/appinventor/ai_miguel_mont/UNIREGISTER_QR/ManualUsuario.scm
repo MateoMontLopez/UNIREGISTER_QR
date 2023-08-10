a192c846cdb2474121712b198f00575ee6c00ce6
MiguelMateoMont <miguel.mont@unipaz.edu.co>
2023-08-10 01:15:34 -0500

    Subida archivos e instaladores APK

---
+++ UNIREGISTER_QR/src/appinventor/ai_miguel_mont/UNIREGISTER_QR/ManualUsuario.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"223","Source":"Form","Properties":{"$Name":"ManualUsuario","$Type":"Form","$Version":"30","AlignHorizontal":"3","AlignVertical":"2","AppName":"UNIREGISTER_QR","BackgroundImage":"AppInterfazGrafica_Fondointerfaz.png","ShowStatusBar":"False","Title":"ManualUsuario","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"VisorWeb1","$Type":"WebViewer","$Version":"10","Height":"-1098","Width":"-1090","Uuid":"607869364"}]}}
+|#
